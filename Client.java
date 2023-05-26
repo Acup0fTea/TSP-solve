b35cc95e9f5a62084e57b96df3f52d005d7ff003
PondPP <93928501+PondPP@users.noreply.github.com>
2023-05-26 21:48:08 +0700

    edit

--- Client.java
+++ Client.java
@@ -3,26 +3,30 @@ import java.net.*;
 
 public class Client {
     public static void main(String[] args) {
+        if (args.length != 8) {
+            System.out.println("Invalid number of arguments. Please provide City Order and weights.");
+            return;
+        }
+
         try {
             Socket socket = new Socket("localhost", 8080);
             System.out.println("Connected to server.");
 
-            // Receive order of cities from server
+            // Send City Order to server
+            String cityOrder = args[0] + "," + args[1] + "," + args[2] + "," + args[3];
+            OutputStream os = socket.getOutputStream();
+            os.write(cityOrder.getBytes());
+            os.flush();
+
+            // Send weights to server
+            String weights = args[4] + "," + args[5] + "," + args[6] + "," + args[7];
+            os.write(weights.getBytes());
+            os.flush();
+
+            // Receive TSP solution from server
             InputStream is = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String line = reader.readLine();
-            int cityOrder = Integer.parseInt(line);
-
-            // Update city order
-            int[] updatedOrder = new int[] { 10 + cityOrder, 20 + cityOrder, 30 + cityOrder, 40 + cityOrder };
-
-            // Send updated order of cities back to server
-            String message = updatedOrder[0] + "," + updatedOrder[1] + "," + updatedOrder[2] + "," + updatedOrder[3];
-            OutputStream os = socket.getOutputStream();
-            os.write(message.getBytes());
-
-            // Receive TSP solution from server
-            line = reader.readLine();
             String[] tspSolution = line.split(",");
             int[] tsp = new int[4];
             for (int i = 0; i < 4; i++) {
@@ -32,7 +36,8 @@ public class Client {
             // Print TSP solution
             System.out.println("TSP Solution:");
             for (int i = 0; i < 4; i++) {
-                System.out.println("City " + (i + 1) + ": " + tsp[i]);
+                System.out.println("City " + tsp[i] + ": " + getShortestPath(args, tsp[i]));
+                System.out.println("Weight: " + getWeight(args, tsp[i]));
             }
 
             // Close connection
@@ -40,5 +45,25 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getShortestPath(String[] args, int city) {
+        StringBuilder path = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            if (Integer.parseInt(args[i]) == city) {
+                path.append((char) ('A' + i));
+                break;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            int nextCity = Integer.parseInt(args[(i + 1) % 4]);
+            path.append(" -> ").append((char) ('A' + nextCity - 1));
+        }
+        return path.toString();
+    }
+
+    private static int getWeight(String[] args, int city) {
+        int index = 4 + city - 1;
+        return Integer.parseInt(args[index]);
     }
 }
--- Server.java
+++ Server.java
@@ -21,20 +21,30 @@ public class Server {
                 os.write(message.getBytes());
             }
 
-            // Receive updated order of cities from clients
+            // Receive updated order of cities and weights from clients
             int[][] cityOrders = new int[4][4];
+            int[][] weights = new int[4][4];
             for (int i = 0; i < 4; i++) {
                 InputStream is = clientSockets[i].getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+                // Receive updated order of cities
                 String line = reader.readLine();
                 String[] order = line.split(",");
                 for (int j = 0; j < 4; j++) {
                     cityOrders[i][j] = Integer.parseInt(order[j]);
                 }
+
+                // Receive weights
+                line = reader.readLine();
+                String[] weightValues = line.split(",");
+                for (int j = 0; j < 4; j++) {
+                    weights[i][j] = Integer.parseInt(weightValues[j]);
+                }
             }
 
-            // Calculate TSP solution
-            int[] tspSolution = calculateTSP(cityOrders);
+            // Calculate TSP solution with weights
+            int[] tspSolution = calculateTSP(cityOrders, weights);
 
             // Send TSP solution back to clients
             for (int i = 0; i < 4; i++) {
@@ -45,18 +55,71 @@ public class Server {
                 OutputStream os = clientSockets[i].getOutputStream();
                 os.write(message.getBytes());
             }
+
+            // Close server socket
+            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static int[] calculateTSP(int[][] cityOrders) {
+    private static int[] calculateTSP(int[][] cityOrders, int[][] weights) {
         // Perform TSP calculation here and return the solution
         // You can use any suitable algorithm for solving TSP
-        // and update the code accordingly
-        // For simplicity, this example just returns the original order
-
+        // Modify the code below to show the TSP calculation
+    
         int[] tspSolution = new int[] { 1, 2, 3, 4 };
+    
+        System.out.println("TSP Calculation:");
+        System.out.println("City Orders: ");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(cityOrders[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Weights: ");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(weights[i][j] + " ");
+            }
+            System.out.println();
+        }
+    
+        // Add your TSP algorithm implementation here
+        // Display intermediate steps as needed
+    
+        System.out.println("Intermediate Steps:");
+        // Add your print statements to show intermediate steps
+    
+        // Final TSP solution
+        System.out.println("TSP Solution:");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("City " + (i + 1) + ": " + tspSolution[i]);
+            System.out.println("Shortest Path: " + getShortestPath(cityOrders[i], tspSolution[i]));
+            System.out.println("Weight: " + getWeight(weights[i], tspSolution[i]));
+            System.out.println();
+        }
+    
         return tspSolution;
+    }
+    
+    private static String getShortestPath(int[] cityOrder, int city) {
+        StringBuilder path = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            if (cityOrder[i] == city) {
+                path.append((char) ('A' + i));
+                break;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            int nextCity = cityOrder[(i + 1) % 4];
+            path.append(" -> ").append((char) ('A' + nextCity - 1));
+        }
+        return path.toString();
+    }
+    
+    private static int getWeight(int[] weight, int city) {
+        return weight[city - 1];
     }
 }
